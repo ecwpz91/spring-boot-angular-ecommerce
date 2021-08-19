ad716d6d0edce4e27a20d765af604df52fd55276
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-08-18 22:58:04 -0400

    Added jaeger tracing

--- src/main/java/com/baeldung/ecommerce/EcommerceApplication.java
+++ src/main/java/com/baeldung/ecommerce/EcommerceApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-// import io.jaegertracing.Configuration;
-// import io.jaegertracing.Configuration.ReporterConfiguration;
-// import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -18,12 +18,12 @@ public class EcommerceApplication {
         SpringApplication.run(EcommerceApplication.class, args);
     }
 
-    // @Bean
-	// public io.opentracing.Tracer initTracer() {
-	// 	SamplerConfiguration samplerConfig = new SamplerConfiguration().withType("const").withParam(1);
-	// 	ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
-	// 	return Configuration.fromEnv("ecommerce").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
-	// }
+    @Bean
+	public io.opentracing.Tracer initTracer() {
+		SamplerConfiguration samplerConfig = new SamplerConfiguration().withType("const").withParam(1);
+		ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
+		return Configuration.fromEnv("ecommerce").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
+	}
 
     @Bean
     CommandLineRunner runner(ProductService productService) {
